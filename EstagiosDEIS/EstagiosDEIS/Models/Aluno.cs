ceacf412d45beb783363bbfcf4ff8f493af7a9ae
Marco Lopes <marcoi.lopes.98@gmail.com>
2018-12-11 14:55:45 +0000

    Aluno e candidatura

---
+++ EstagiosDEIS/EstagiosDEIS/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstagiosDEIS.Models
+{
+    public class Aluno
+    {
+        public int NumeroAluno { get; set; }
+        public String NomeAluno { get; set; }
+        public Estagio estagio { get; set; }
+        public Candidatura candidatura { get; set; }
+
+        public Aluno(int numero, String nome)
+        {
+            this.NumeroAluno = numero;
+            this.NomeAluno = nome;
+        }
+    }
+}
---
+++ EstagiosDEIS/EstagiosDEIS/Models/Candidatura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstagiosDEIS.Models
+{
+    public enum Ramo
+    {
+        DesenvolvimentodeAplicações,
+        SistemasdeInformação,
+        RedeseAdministraçãodeSistemas
+    }
+    
+    public enum Estado
+    {
+        Aceite,
+        EsperaResultado,
+        PrecisaEntrevista,
+        Rejeitado
+    }
+
+    public class Candidatura
+    {
+        public Ramo ramo { get; set; }
+        public Estado estado { get; set; }
+        public String LocalPreferencia { get; set; }
+        public String NomeOrientador { get; set; }
+        public int NumeroOrientador { get; set; }
+        public DateTime DataDeCandidatura { get; set; } 
+
+    }
+}
